885600fdfff02fc36ee45ebb2e5f7f1c144d0b19
Gergo Kovacs <gergosmith306@gmail.com>
2023-05-02 16:05:40 +0200

    Add starter Spring Boot class, test and properties

---
+++ src/main/java/com/geri/notesapp/NotesappApplication.java
@@ -0,0 +1,13 @@
+package com.geri.notesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesappApplication.class, args);
+	}
+
+}

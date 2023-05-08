1bd56cd111610326f0c536269cd0d4d206129d5e
Gergo Kovacs <gergosmith306@gmail.com>
2023-05-08 10:20:35 +0200

    Add custom exceptions

---
+++ backend/main/java/com/codecool/api/exception/NoteNotFoundException.java
@@ -0,0 +1,28 @@
+package com.codecool.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NoteNotFoundException extends Exception {
+    // disable stack trace
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+
+    public NoteNotFoundException() {
+    }
+
+    public NoteNotFoundException(String message) {
+        super(message);
+    }
+
+    public NoteNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoteNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}

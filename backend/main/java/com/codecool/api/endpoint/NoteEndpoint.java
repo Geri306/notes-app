76f153382ca39b6c8719d642d632cae929e9eb7c
Gergo Kovacs <gergosmith306@gmail.com>
2023-05-15 12:11:10 +0200

    Add lomobk-slf4j, add method calls: format date string, reset id sequence

--- backend/main/java/com/codecool/api/endpoint/NoteEndpoint.java
+++ backend/main/java/com/codecool/api/endpoint/NoteEndpoint.java
@@ -4,10 +4,10 @@ import com.codecool.api.exception.NoteNotFoundException;
 import com.codecool.logic.NoteService;
 import com.codecool.persistence.entity.Note;
 import com.codecool.persistence.repository.NoteRepository;
+import com.codecool.persistence.repository.QueryRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,9 +19,10 @@ import java.util.Optional;
 @RestController
 @RequestMapping("api/v1/notes/")
 @AllArgsConstructor
+@Slf4j
 public class NoteEndpoint {
 
-    private final Logger log = LoggerFactory.getLogger(NoteEndpoint.class);
+    //TODO migrate all calls to noteRepository to noteService
     private final NoteRepository noteRepository;
     private final NoteService noteService;
 
@@ -29,45 +30,59 @@ public class NoteEndpoint {
     ResponseEntity<Note> create() throws URISyntaxException {
         log.info("Request to create new note");
         Note result = noteRepository.save(new Note());
-        return ResponseEntity.created(new URI("api/v1/notes/get/" + result.getId()))
+        return ResponseEntity
+                .created(new URI("api/v1/notes/get/" + result.getId()))
                 .body(result);
     }
+
+    // TEST START
+
+/*    @RequestMapping("/handle")
+    @PostMapping
+    public ResponseEntity<String> handle() throws URISyntaxException {
+//        URI location = new URI("api/mydocs");
+//        HttpHeaders responseHeaders = new HttpHeaders();
+//        responseHeaders.setLocation(location);
+//        responseHeaders.set("MyResponseHeader", "MyValue");
+//        return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
+        URI location = new URI("api/mydocs");
+        return ResponseEntity.created(location).header("MyResponseHeader", "MyValue").body("Hello World");
+    }*/
+
+    // TEST END
 
     @GetMapping("get/{id}")
     ResponseEntity<?> getOneById(@PathVariable Long id) throws NoteNotFoundException {
         Optional<Note> result = noteRepository.findById(id);
         return result
-                .map(note -> ResponseEntity.ok().body(note))
-                .orElseThrow(
-                        () -> new NoteNotFoundException(
-                                "cannot find note with id: " + id
-                        )
-                );
+                .map(note -> {
+                    Note formattedNote = noteService.formatDate(note);
+                    return ResponseEntity.ok().body(formattedNote);
+                })
+                .orElseThrow(() -> new NoteNotFoundException(
+                        "cannot find note with id: " + id));
     }
 
     @GetMapping("get/all")
     List<Note> readAll() {
+        noteRepository.findAll().forEach(noteService::formatDate);
         return noteRepository.findAll();
     }
 
     @PutMapping("put/{id}")
     ResponseEntity<Note> replaceNote(@Valid @RequestBody Note newNote, @PathVariable long id) {
-        System.out.println("ID = " + id);
-        System.out.println("newNote = " + newNote);
         log.info("Request to update note with id: " + id);
         return noteRepository.findById(id)
                 .map(oldNote -> {
-                            Note updatedNote = noteService.getUpdatedNote(oldNote, newNote);
-                            Note result = noteRepository.save(updatedNote);
-                            return ResponseEntity.ok().body(result);
-                        }
-                )
+                    Note updatedNote = noteService.getUpdatedNote(oldNote, newNote);
+                    Note result = noteRepository.save(updatedNote);
+                    return ResponseEntity.ok().body(result);
+                })
                 .orElseGet(() -> {
-                            newNote.setId(id);
-                            Note result = noteRepository.save(newNote);
-                            return ResponseEntity.ok().body(result);
-                        }
-                );
+                    newNote.setId(id);
+                    Note result = noteRepository.save(newNote);
+                    return ResponseEntity.ok().body(result);
+                });
     }
 
     @PutMapping("put/nextlabel/{id}")
@@ -75,16 +90,14 @@ public class NoteEndpoint {
         log.info("Request to change label on note with id: " + id);
         return noteRepository.findById(id)
                 .map(note -> {
-                            Note updatedNote = noteService.getNoteWithNextLabel(note);
-                            noteRepository.save(updatedNote);
-                            return ResponseEntity.ok().body(updatedNote);
-                        }
-                )
+                    Note updatedNote = noteService.getNoteWithNextLabel(note);
+                    noteRepository.save(updatedNote);
+                    return ResponseEntity.ok().body(updatedNote);
+                })
                 .orElseThrow(
                         () -> new IllegalArgumentException(
                                 "Label change request for non-existing note with id: " + id
-                        )
-                );
+                        ));
 
     }
 
@@ -99,6 +112,7 @@ public class NoteEndpoint {
     public ResponseEntity<?> deleteAll() {
         log.info("Request to delete all notes");
         noteRepository.deleteAll();
+        noteRepository.resetSequence();
         return ResponseEntity.ok().build();
     }
 }

baed263cae991d4018a3580de9f2a773bfe71c6e
Gergo Kovacs <gergosmith306@gmail.com>
2023-05-15 12:13:45 +0200

    Add method to service to format date for nice client side rendering

--- backend/main/java/com/codecool/logic/NoteService.java
+++ backend/main/java/com/codecool/logic/NoteService.java
@@ -5,8 +5,11 @@ import com.codecool.persistence.entity.Note;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
-@NoArgsConstructor
+@NoArgsConstructor //TODO: change to @RequiredArgsConstructor after migrating methods
 public class NoteService {
     public Note getUpdatedNote(Note oldNote, Note newNote) {
         newNote.setId(oldNote.getId());
@@ -18,6 +21,13 @@ public class NoteService {
         int nextIndex = Label.valueOf(note.getLabel().toString()).ordinal() + 1;
         Label nextLabel = Label.values()[nextIndex % limit];
         note.setLabel(nextLabel);
+        return note;
+    }
+
+    public Note formatDate(Note note) {
+        LocalDateTime old = note.getModified_L();
+        String formatted = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm").format(old);
+        note.setModified(formatted);
         return note;
     }
 }

75f50f91f8accb203b7cdcc4dceaee2aa00e8431
Gergo Kovacs <gergosmith306@gmail.com>
2023-05-15 12:08:08 +0200

    Add equals, hashCode; refactor annotations; add field to store date as string for rendering

--- backend/main/java/com/codecool/persistence/entity/Note.java
+++ backend/main/java/com/codecool/persistence/entity/Note.java
@@ -1,39 +1,61 @@
 package com.codecool.persistence.entity;
 
 import com.codecool.data.Label;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+
+// consider removing @Data and @EqualsAndHashCode annotations ...
+// ... in JPA entity classes due to performance issues
 @Entity
 @Getter
 @Setter
-@Table(name = "note")
+@Table
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@EqualsAndHashCode
 public class Note {
-    @Id
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "note_id_sequence" // default: "note_seq"
+    )
     @SequenceGenerator(
             name = "note_id_sequence",
             sequenceName = "note_id_sequence",
             allocationSize = 1
     )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "note_id_sequence"
-    )
+    @Id
     private long id;
-    @Column(name = "content")
+    private boolean done = false;
+    /*@Column(name = "content")*/
     private String content = "";
-    @Column(name = "label")
+    // without @Enumerated(...) the label would have a numerical value in the database
     @Enumerated(value = EnumType.STRING)
     private Label label = Label.GREEN;
-    @Column(name = "done")
-    private boolean done = false;
-    @Column(name = "modified")
-    private LocalDateTime modified = LocalDateTime.now();
+    @UpdateTimestamp
+    @JsonIgnore
+    private LocalDateTime modified_L;
+    // To ignore a field, annotate it with @Transient so it will not be mapped by hibernate.
+    @Transient
+    private String modified;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return id == note.id && done == note.done && Objects.equals(content, note.content) && label == note.label && Objects.equals(modified_L, note.modified_L) && Objects.equals(modified, note.modified);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, label, done, modified_L, modified);
+    }
 }

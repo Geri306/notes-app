4c65ceb8f1c58f1d24b827ee319b78663cd74e18
Gergo Kovacs <gergosmith306@gmail.com>
2023-05-15 12:12:59 +0200

    Extend repo interface with new interface to reset id sequence

--- backend/main/java/com/codecool/persistence/repository/NoteRepository.java
+++ backend/main/java/com/codecool/persistence/repository/NoteRepository.java
@@ -2,6 +2,9 @@ package com.codecool.persistence.repository;
 
 import com.codecool.persistence.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface NoteRepository extends JpaRepository<Note, Long> {
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long>, QueryRepository {
+
 }

58b95814b0bdaa3683f118d32ae736d0a12feccf
Gergo Kovacs <gergosmith306@gmail.com>
2023-05-15 17:41:51 +0200

    Change parent class of custom exception

--- backend/main/java/com/codecool/api/exception/NoteNotFoundException.java
+++ backend/main/java/com/codecool/api/exception/NoteNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NoteNotFoundException extends Exception {
+public class NoteNotFoundException extends RuntimeException {
     // disable stack trace
     @Override
     public synchronized Throwable fillInStackTrace() {

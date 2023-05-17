b335c4bfe47a5fe8a5bcc178bf04452cf9c5a8ec
Gergo Kovacs <gergosmith306@gmail.com>
2023-05-17 13:25:40 +0200

    Write new query method

--- backend/main/java/com/codecool/notes/persistence/repository/NoteRepository.java
+++ backend/main/java/com/codecool/notes/persistence/repository/NoteRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long>, QueryRepository {
 
     List<Note> findByDoneTrue();
+
+    Optional<Note> findByContent(String content);
 
 }
